35014133a1a2eebed8379f8de668a0bdc8aeee7d
SergNot <s.notevskiy@gmail.com>
2019-02-17 18:39:42 +0200

    добавил логирование

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.common import exceptions
 from selenium.webdriver.support.wait import WebDriverWait
+import logging
+
+logging.basicConfig(level=logging.INFO, filename='log.log', filemode='w',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Application:
@@ -9,7 +13,10 @@ class Application:
         self.wd.implicitly_wait(2)
         self.exceptions = exceptions
         self.waits = WebDriverWait
+        self.log = logging
+        self.log.info('Запуск тестов')
 
     def destroy(self):
-        pass
+        self.wd.quit()
+        self.log.info('Тесты завершены')
 
